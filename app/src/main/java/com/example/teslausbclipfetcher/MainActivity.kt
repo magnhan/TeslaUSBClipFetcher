ede30a03d351be406fcf2cb508116b6b074fff99
magnhan <45832668+magnhan@users.noreply.github.com>
2020-07-02 15:30:58 +0200

    Updated API support
    Now follows linter practices as well

--- app/src/main/java/com/example/teslausbclipfetcher/MainActivity.kt
+++ app/src/main/java/com/example/teslausbclipfetcher/MainActivity.kt
@@ -18,57 +18,61 @@ class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
-        println("klar som et egg")
-        FuelManager.instance.basePath = "http://www.teslausb-281110.ey.r.appspot.com";
+        FuelManager.instance.basePath = "http://www.teslausb-281110.ey.r.appspot.com"
     }
-    fun httpPostJson(view: View){
+
+    fun httpPostJson(view: View) {
+        println(editVIN.text)
         try {
             Fuel.post(
-                "json")
-                .jsonBody("{ \"car_name\" : \"${editVIN.text}\" }")
-                .response {result ->
+                "json"
+            )
+                .jsonBody(
+                    "{\"car_name\" : \"${editVIN.text}\"}"
+                )
+                .response { result ->
                     val (bytes, error) = result
                     if (bytes != null) {
                         val clips = Gson().fromJson(String(bytes), Map::class.java)
                         print(clips)
-                        var savedClips = listOf<String>()
-                        var sentryClips = listOf<String>()
-                        if(clips["SavedClips"] != "det er ingen savedclips til: ${editVIN.text}"){
-                            savedClips = clips["SavedClips"] as List<String>
+                        @Suppress("UNCHECKED_CAST")
+                        val savedClips = clips["SentryClips"] as List<String>
+                        @Suppress("UNCHECKED_CAST")
+                        val sentryClips = clips["SentryClips"] as List<String>
+                        if (savedClips.isNotEmpty()) {
+                            for (video in savedClips) {
+                                println("Kaller funksjon for $video")
+                                addVideoView(video)
+                            }
                         }
-                        if(clips["SentryClips"] != "det er ingen sentry_clips til: ${editVIN.text}") {
-                            sentryClips = clips["SentryClips"] as List<String>
-                        }
-                        for(video in savedClips){
-                            println("Kaller funksjon for $video")
-                            addVideoView(video)
+                        if (sentryClips.isNotEmpty()) {
+                            for (video in sentryClips) {
+                                println("Kaller funksjon for $video")
+                                addVideoView(video)
+                            }
                         }
                     }
                 }
-        } catch (e: Exception){
-            println("ting er fucka opp ${e.message}")
+        } catch (e: Exception) {
+            println(e.message)
         }
     }
-    private fun addVideoView(url: String){
-        println("Legger inn $url")
+
+    private fun addVideoView(url: String) {
         val videoView = VideoView(this)
         val params = LinearLayout.LayoutParams(
             ViewGroup.LayoutParams.MATCH_PARENT,
             ViewGroup.LayoutParams.WRAP_CONTENT
         )
-        params.setMargins(10,0,10,10)
+        params.setMargins(10, 0, 10, 10)
         videoView.layoutParams = params
         val uri: Uri = Uri.parse("http://techslides.com/demos/sample-videos/small.mp4")
         videoView.setVideoPath("https://tiger.cdnja.co/v/qp/QpyaV.mp4?secure=H3i0G1KyG_N8yaNwIix7lg&expires=1593678600")
-        youtubeView.setVideoURI(uri)
+        val youtube: VideoView = findViewById(R.id.youtubeView)
+        youtube.setVideoURI(uri)
         println("video satt")
-        //videoView.start()
+        // videoView.start()
         val linearLayout: LinearLayout = findViewById(R.id.root_layout)
-        //linearLayout.addView(videoView)
-    }
-    fun times(){
-        //savedClips.forEach(addVideoView())
+        // linearLayout.addView(videoView)
     }
 }
-
-
